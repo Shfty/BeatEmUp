5716d8147c617e9d75a11434530cf21f974aa62f
Josh Palmer <jpalmerwatkins@me.com>
2017-05-09 00:16:16 +0100

    Rebindable input via options menu

--- Source/PunchGame/Private/BeatEmUpStatics.cpp
+++ Source/PunchGame/Private/BeatEmUpStatics.cpp
@@ -35,6 +35,199 @@ TArray<FString> UBeatEmUpStatics::GetAllMapNames()
 	return GetAllMapNamesInFolder("");
 }
 
+TArray<FName> UBeatEmUpStatics::GetInputAxisNames()
+{
+	TArray<FName> AxisNames;
+
+	const UInputSettings* InputSettings = GetDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		InputSettings->GetAxisNames(AxisNames);
+	}
+
+	return AxisNames;
+}
+
+TArray<FName> UBeatEmUpStatics::GetInputActionNames()
+{
+	TArray<FName> AxisNames;
+
+	const UInputSettings* InputSettings = GetDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		InputSettings->GetActionNames(AxisNames);
+	}
+
+	return AxisNames;
+}
+
+TArray<FInputAxisKeyMapping> UBeatEmUpStatics::GetInputAxisMappings()
+{
+	const UInputSettings* InputSettings = GetDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		return InputSettings->AxisMappings;
+	}
+
+	TArray<FInputAxisKeyMapping> Empty;
+	return Empty;
+}
+
+TArray<FInputActionKeyMapping> UBeatEmUpStatics::GetInputActionMappings()
+{
+	const UInputSettings* InputSettings = GetDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		return InputSettings->ActionMappings;
+	}
+
+	TArray<FInputActionKeyMapping> Empty;
+	return Empty;
+}
+
+TArray<FKey> UBeatEmUpStatics::GetInputKeysForAxis(FName Axis)
+{
+	const UInputSettings* InputSettings = GetDefault<UInputSettings>();
+
+	TArray<FKey> Keys;
+
+	if(InputSettings != nullptr)
+	{
+		for(auto AxisMapping : InputSettings->AxisMappings)
+		{
+			if(AxisMapping.AxisName == Axis)
+			{
+				Keys.Add(AxisMapping.Key);
+			}
+		}
+	}
+
+	return Keys;
+}
+
+TArray<FKey> UBeatEmUpStatics::GetInputKeysForAction(FName Action)
+{
+	const UInputSettings* InputSettings = GetDefault<UInputSettings>();
+
+	TArray<FKey> Keys;
+
+	if(InputSettings != nullptr)
+	{
+		for(auto ActionMapping : InputSettings->ActionMappings)
+		{
+			if(ActionMapping.ActionName == Action)
+			{
+				Keys.Add(ActionMapping.Key);
+			}
+		}
+	}
+
+	return Keys;
+}
+
+void UBeatEmUpStatics::SetInputAxisMappings(TArray<FInputAxisKeyMapping> NewAxisMappings)
+{
+	UInputSettings* InputSettings = GetMutableDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		InputSettings->AxisMappings.Empty();
+		InputSettings->AxisMappings.Append(NewAxisMappings);
+		InputSettings->SaveConfig();
+	}
+}
+
+void UBeatEmUpStatics::SetInputActionMappings(TArray<FInputActionKeyMapping> NewActionMappings)
+{
+	UInputSettings* InputSettings = GetMutableDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		InputSettings->ActionMappings.Empty();
+		InputSettings->ActionMappings.Append(NewActionMappings);
+		InputSettings->SaveConfig();
+	}
+}
+
+void UBeatEmUpStatics::RemapInputAxis(FName Axis, FKey OldKey, FKey NewKey)
+{
+	if(Axis.IsNone())
+	{
+		return;
+	}
+
+	UInputSettings* InputSettings = GetMutableDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		for(auto& AxisMapping : InputSettings->AxisMappings)
+		{
+			if(AxisMapping.AxisName == Axis && AxisMapping.Key == OldKey)
+			{
+				AxisMapping.Key = NewKey;
+			}
+		}
+
+		InputSettings->SaveConfig();
+	}
+}
+
+void UBeatEmUpStatics::RemapInputAction(FName Action, FKey OldKey, FKey NewKey)
+{
+	if(Action.IsNone())
+	{
+		return;
+	}
+
+	UInputSettings* InputSettings = GetMutableDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		if(InputSettings->ActionMappings.FindByPredicate([Action, NewKey](FInputActionKeyMapping Mapping) { return Mapping.ActionName == Action && Mapping.Key == NewKey; }) != nullptr)
+		{
+			return;
+		}
+
+		TArray<FInputActionKeyMapping> InputActions;
+		for(auto& ActionMapping : InputSettings->ActionMappings)
+		{
+			if(ActionMapping.ActionName == Action && ActionMapping.Key == OldKey)
+			{
+				ActionMapping.Key = NewKey;
+			}
+		}
+
+		InputSettings->SaveConfig();
+	}
+}
+
+
+void UBeatEmUpStatics::ClearInputAxisMappings()
+{
+	UInputSettings* InputSettings = GetMutableDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		InputSettings->AxisMappings.Empty();
+		InputSettings->SaveConfig();
+	}
+}
+
+void UBeatEmUpStatics::ClearInputActionMappings()
+{
+	UInputSettings* InputSettings = GetMutableDefault<UInputSettings>();
+
+	if(InputSettings != nullptr)
+	{
+		InputSettings->ActionMappings.Empty();
+		InputSettings->SaveConfig();
+	}
+}
+
 bool UBeatEmUpStatics::ValidateIPAddress(FString IPAddress)
 {
 	if(IPAddress.IsEmpty())
--- Source/PunchGame/Public/BeatEmUpStatics.h
+++ Source/PunchGame/Public/BeatEmUpStatics.h
@@ -20,6 +20,42 @@ public:
 	UFUNCTION(BlueprintPure, Category = "Utility")
 	static TArray<FString> GetAllMapNames();
 
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static TArray<FName> GetInputAxisNames();
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static TArray<FName> GetInputActionNames();
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static TArray<FInputAxisKeyMapping> GetInputAxisMappings();
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static TArray<FInputActionKeyMapping> GetInputActionMappings();
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static TArray<FKey> GetInputKeysForAxis(FName Axis);
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static TArray<FKey> GetInputKeysForAction(FName Action);
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static void SetInputAxisMappings(TArray<FInputAxisKeyMapping> NewAxisMappings);
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static void SetInputActionMappings(TArray<FInputActionKeyMapping> NewActionMappings);
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static void RemapInputAxis(FName Axis, FKey OldKey, FKey NewKey);
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static void RemapInputAction(FName Action, FKey OldKey, FKey NewKey);
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static void ClearInputAxisMappings();
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	static void ClearInputActionMappings();
+
 	UFUNCTION(BlueprintPure, Category = "Utility")
 	static bool ValidateIPAddress(FString IPAddress);
 	
